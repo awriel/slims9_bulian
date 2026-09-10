b847dd1b88d395f31ab56c0663d848aba2d5bdec
Aril Apria Susanto <awriel@gmail.com>
2026-09-10 07:03:08 +0700

    Replace member_type with inst_name field on circulation

--- admin/modules/circulation/circulation_action.php
+++ admin/modules/circulation/circulation_action.php
@@ -559,8 +559,10 @@ if (isset($_POST['memberID']) OR isset($_SESSION['memberID'])) {
         $html .= '<tr>'."\n";
         $html .= '<td class="alterCell" width="15%"><strong>'.__('Member Email').'</strong></td>'
                . '<td class="alterCell2" width="30%">'.$member->member_email.'</td>';
-        $html .= '<td class="alterCell" width="15%"><strong>'.__('Member Type').'</strong></td>'
-               . '<td class="alterCell2" width="30%">'.$member->member_type_name.'</td>';
+        //$html .= '<td class="alterCell" width="15%"><strong>'.__('Member Type').'</strong></td>'
+        //       . '<td class="alterCell2" width="30%">'.$member->member_type_name.'</td>';
+        $html .= '<td class="alterCell" width="15%"><strong>'.__('Institution').'</strong></td>'
+               . '<td class="alterCell2" width="30%">'.$member->inst_name.'</td>';
         $html .= '</tr>'."\n";
 
         // register / expiry row
